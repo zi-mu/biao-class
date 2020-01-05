e115f34c5bb08a3c49d75e18f9a5b0980912a788
zi-mu <zimu.joker@gmail.com>
2020-01-05 19:17:58 +0800

    032-JS不仅是 神经,注释，引用和严格模式

---
+++ practice/032-JS不仅是神经/main.js
@@ -0,0 +1,30 @@
+// ;
+// (function () {
+//     'use strict';
+//     console.log('haha');
+// })();
+
+
+// ;
+// (function () {
+//     'use strict';
+//     let a = 2;
+//     console.log(a);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+//     console.log('hello,world!')
+// })();
+
+;
+(function () {
+    'use strict';
+
+    let h1 = document.createElement('h1');
+    h1.innerText = 'hello world';
+    h1.style.color = 'red';
+    document.body.appendChild(h1);
+
+})();
