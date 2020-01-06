edec2a73f4143799aa41ad4791f448dd5d5dfd46
zi-mu <zimu.joker@gmail.com>
2020-01-06 22:28:29 +0800

    变量和常量

---
+++ practice/033-变量和常量/main.js
@@ -0,0 +1,255 @@
+// ;
+// (function () {
+//     'use strict';
+
+//     // let a = 1;
+//     // let b = 1;
+//     // let a, b;
+//     // a = 1;
+//     // b = 1;
+
+//     let a, b;
+//     a = 1;
+//     b = 2;
+
+//     a = 100;
+//     b = 200;
+
+//     console.log('a:', a);
+//     console.log('b:', b);
+// })();
+
+// 1.
+// ;
+// (function () {
+//'use strict'
+//     let name, nickname;
+//     name = 'wanghuahua';
+//     nickname = name;
+//     console.log(name,nickname);
+// })();
+//2
+// ;
+// (function () {
+//     'use strict';
+
+//     let name, nickname;
+//     name = 'lsd';
+//     nickname = name;
+//     console.log(name,nickname);
+// })();
+// 3
+// ;
+// (function () {
+//     'use strict';
+
+//     let name, nickname;
+//     name = 'gbk';
+//     nickname = name;
+//     console.log(name,nickname);
+// })();
+
+//4.
+// ;
+// (function () {
+//     'use strict';
+
+//     let name, nickname;
+//     name = 'fanxian';
+//     nickname = name;
+//     console.log(name,nickname);
+// })();
+
+//5
+// ;
+// (function () {
+//     'use strict';
+//     let name, nickname;
+//     name = 'fanjian';
+//     nickname = name;
+//     console.log(name,nickname);
+// })();
+
+//6.
+// ;
+// (function () {
+//     'use strict';
+
+//     let name, nickname;
+//     name = 'qingdi';
+//     nickname = name;
+//     console.log(name,nickname);
+//     
+// })();
+//7.
+// ;
+// (function () {
+//     'use strict';
+
+//     let name, nickname;
+//     name = 'fansize';
+//     nickname = name;
+//     console.log(name,nickname);
+//   
+// })();
+//8.
+// ;
+// (function () {
+//     'use strict';
+
+//     let name, nickname;
+//     name = 'susu';
+//     nickname = name;
+//     console.log(name,nickname);
+//     
+// })();
+
+//9.
+// ;
+// (function () {
+//     'use strict';
+//     let name, nickname;
+//     name = 'pingping';
+//     nickname = name;
+//     console.log(name,nickname);
+//    
+// })();
+//10.
+// ;
+// (function () {
+//     'use strict';
+
+//     let name, nickname;
+//     name = 'youyou';
+//     nickname = name;
+//     console.log(name, ',', nickname);
+// })();
+
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = a + b;
+// console.log(a, b, c);
+
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = a + b;
+// console.log(a, b, c);
+
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = a + b;
+// console.log(a, b, c);
+
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = a + b;
+// console.log(a, b, c);
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = a + b;
+// console.log(a, b, c);
+
+// let a, b, c;
+// a = 2;
+// b = 2;
+// c = a + b;
+// console.log(a, b, c);
+
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = a + b;
+// console.log(a, b, c);
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let a, b, c;
+//     a = 1;
+//     b = 2;
+//     c = a + b;
+//     console.log(a, b, c);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let a, b, c;
+//     a = 1;
+//     b = 2;
+//     c = a + b;
+//     console.log(a, b, c);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let a, b, c;
+//     a = 2;
+//     b = 2;
+//     c = a + b;
+//     console.log(a, b, c);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let a, b, c;
+//     a = 1;
+//     b = 2;
+//     c = a + b;
+//     console.log(a, b, c);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     const A = 'lsd';
+//     const B = 'ltz';
+//     console.log(A, B);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     const Pi = 3.14;
+//     let a = 2;
+//     console.log(Pi * a);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     const Name = 'tian ';
+//     const Address = 'henan';
+//     console.log(Name + 'live in ' + Address)
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     const NAME = '王花花';
+//     let a = ' ,你好!';
+//     console.log(NAME + a);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     const A = 1;
+//     A = 2;
+// })();
