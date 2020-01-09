c8639c13e7c278af05063be5451ec03f8e886267
zi-mu <zimu.joker@gmail.com>
2020-01-09 23:39:47 +0800

    loop 90%

---
+++ practice/037-所有重复的工作豆可以交给循环(for,while)/main.js
@@ -0,0 +1,356 @@
+;
+(function () {
+    'use strict';
+
+    // let userList = [
+    //     '王花花',
+    //     '李拴蛋',
+    //     '赵克爽',
+    // ];
+    // for (let i = 0; i < userList; i++) {
+    //     console.log(userList[i]);
+    // }
+
+    //2.
+    // let userList = [
+    //     '王花花',
+    //     '李拴蛋',
+    //     '赵克爽',
+    // ];
+
+    // for (let i = 0; i < userList.length; i++) {
+    //     let it = userList[i];
+    //     if (it === '李拴蛋')
+    //         continue;
+    //     console.log(it);
+    // }
+
+
+    // let userList = [
+    //     '王花花',
+    //     '李拴蛋',
+    //     '赵克爽',
+    // ];
+    // let str = '';
+    // for (let i = 0; i < userList.length; i++) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user == '赵克爽') {
+    //         str = str.slice(0, -1);
+    //     }
+
+    // }
+    // let str = '';
+    // for (let i = 0; i < userList.length; i++) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽') {
+    //         str = str.slice(0, -1);
+    //     }
+    // }
+    // let str = '';
+    // for (let i = 0; i < userList.length; i++) {
+    //     let user = userList[i];
+    //     str = str + user + ',';
+    //     if (user === '赵克爽') {
+    //         str = str.slice(0, -1);
+    //     }
+    // }
+
+    // let str = '';
+    // for (let i = 0; i < userList.length; i++) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽')
+    //         str = str.slice(0, -1);
+    // }
+    // let str = '';
+    // for (let i = 0; i < userList.length; i++) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽')
+    //         str = str.slice(0, -1);
+    // }
+    // let i = 0;
+    // let str = '';
+    // while (i < userList.length) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽')
+    //         str = str.slice(0, -1);
+    //     i++;
+    // }
+    // console.log(str);
+
+    // let i = 0;
+    // let str = '';
+    // while (i < userList.length) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽')
+    //         str = str.slice(0, -1);
+    //     i++;
+    // }
+    // console.log(str);
+    // let i = 0;
+    // let str = '';
+    // while (i < userList.length) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽')
+    //         str = str.slice(0, -1);
+    //     i++
+    // }
+    // console.log(str);
+
+    // let i = 0;
+    // let str = '';
+    // while (i < userList.length) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽')
+    //         str = str.slice(0, -1);
+    //     i++
+    // }
+    // console.log(str);
+
+    // let i = 0;
+    // let str = '';
+    // while (i < userList.length) {
+    //     let user = userList[i];
+    //     str += user + ',';
+    //     if (user === '赵克爽')
+    //         str = str.slice(0, -1);
+    //     i++
+    // }
+    // console.log(str);
+})();
+
+;
+(
+    function () {
+        'user strict';
+
+
+        // let arr = [
+        //     '王花花',
+        //     '刘贝贝',
+        //     '李拴蛋',
+        //     '赵克爽',
+        // ];
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === '李拴蛋')
+        //         break;
+        //     console.log(arr[i]);
+        // }
+
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === '李拴蛋')
+        //         break;
+        //     console.log(arr[i]);
+        // }
+        // for (let i = 0; i < arr.length; i++) {
+        //     let user = arr[i];
+        //     if (user === '李拴蛋')
+        //         break;
+        //     console.log(user);
+        // }
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === '李拴蛋')
+        //         break;
+        //     console.log(arr[i]);
+        // }
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === '李拴蛋')
+        //         break;
+        //     console.log(arr[i]);
+        // }
+
+        // for (let i = 0; i < arr.length; i++) {
+        //     let user = arr[i];
+        //     if (user === '李拴蛋')
+        //         break;
+        //     console.log(user);
+        // }
+        // let arr = [
+        //     '王花花',
+        //     '刘贝贝',
+        //     '李拴蛋',
+        //     '赵克爽',
+        // ];
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === '李拴蛋')
+        //         continue;
+        //     console.log(arr[i]);
+        // }
+
+        // for (let i = 0; i < arr.length; i++) {
+        //     let it = arr[i];
+        //     if (it === '李拴蛋')
+        //         continue;
+        //     console.log(it);
+        // }
+
+        //     for (let i = 0; i < arr.length; i++) {
+        //         let it = arr[i];
+
+        //         if (it === '李拴蛋')
+        //             continue;
+
+        //         console.log(it);
+        // }
+
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === '李拴蛋')
+        //         continue;
+        //     console.log(arr[i]);
+        // }
+
+        // for (let i = 0; i < arr.length; i++) {
+        //     if (arr[i] === '李拴蛋')
+        //         continue;
+        //     console.log(arr[i]);
+        // }
+
+        // let arr = [
+        //     '王花花',
+        //     '李拴蛋',
+        //     '赵克爽',
+        // ];
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     let user = arr[i];
+        //     str += user;
+        // }
+        // console.log(str);
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     str += arr[i];
+        // }
+        // // console.log(str);
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     let it = arr[i];
+
+        //     str += it;
+        // }
+        // console.log(str);
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     str += arr[i];
+        // }
+        // console.log(str);
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     str += arr[i];
+        // }
+        // console.log(str);
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     str += arr[i] + ',';
+        //     if (arr[i] === '赵克爽')
+        //         str = str.slice(0, -1);
+        // }
+        // console.log(str);
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     let user = arr[i];
+        //     let connector = ',';
+
+        //     if (i == arr.length - 1)
+        //         connector = '';
+
+        //     str += user + connector;
+
+        // }
+        // console.log(str);
+
+        // let str = '';
+        // let length = arr.length;
+        // for (let i = 0; i < length; i++) {
+        //     let it = arr[i];
+        //     let connector = ',';
+
+        //     if (i == length - 1)
+        //         connector = '';
+
+        //     str += it + connector;
+        // }
+        // console.log(str);
+
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     let it = arr[i];
+        //     let connector = ',';
+
+        //     if (i == arr.length - 1)
+        //         connector = '';
+
+        //     str += it + connector;
+        // }
+        // console.log(str);
+
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     let connector = ',';
+        //     if (i == arr.length - 1)
+        //         connector = '';
+
+        //     str += arr[i] + connector;
+        // }
+        // console.log(str);
+
+        // let str = '';
+        // for (let i = 0; i < arr.length; i++) {
+        //     let connector = ',';
+        //     if (i == arr.length - 1)
+        //         connector = '';
+        //     str = str + arr[i] + connector;
+        // }
+        // console.log(str);
+        // let sum = 0;
+        // let list = [1, 4, 6, 9, 10];
+        // for (let i = 0; i < list.length; i++) {
+        //     sum += list[i];
+        // }
+        // console.log(sum);
+        // let sum = 0;
+        // let list = [1, 4, 6, 9, 10];
+        // for (let i = 0; i < list.length; i++) {
+        //     sum = sum + list[i];
+        // }
+        // console.log(sum);
+        // let sum = 0;
+        // let list = [1, 4, 6, 9, 10];
+        // for (let i = 0; i < list.length; i++) {
+        //     sum += list[i];
+        // }
+        // console.log(sum);
+        // let sum = 0;
+        // let list = [1, 4, 6, 9, 10];
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     sum += it;
+        // }
+        // console.log(sum);
+        let sum = 0;
+        let str = '';
+        //1.找出所有数字,并相加.
+        let list = ['Yo', 'Em', 1, 'La', 4, 8, 'Ha', 11, 5];
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'number')
+        //         continue;
+        //     sum += it;
+        // }
+        // console.log(sum);
+        //2. 找出所有字符串,连接
+        for (let i = 0; i < list.length; i++) {
+            let it = list[i];
+            if (typeof it != 'string')
+                continue;
+
+            str += it;
+        }
+        console.log(str);
+    })();
