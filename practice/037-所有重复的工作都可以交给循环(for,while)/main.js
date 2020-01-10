60464ed8625fd5b09a12faa0bc566331a4905fd6
zi-mu <zimu.joker@gmail.com>
2020-01-10 23:52:23 +0800

    loop 100%

--- practice/037-所有重复的工作都可以交给循环(for,while)/main.js
+++ practice/037-所有重复的工作都可以交给循环(for,while)/main.js
@@ -333,10 +333,11 @@
         //     sum += it;
         // }
         // console.log(sum);
-        let sum = 0;
-        let str = '';
+        // let sum = 0;
+        // let str = '';
+        // let list = ['Yo', 'Em', 1, 'La', 4, 8, 'Ha', 11, 5];
+
         //1.找出所有数字,并相加.
-        let list = ['Yo', 'Em', 1, 'La', 4, 8, 'Ha', 11, 5];
         // for (let i = 0; i < list.length; i++) {
         //     let it = list[i];
         //     if (typeof it != 'number')
@@ -344,13 +345,683 @@
         //     sum += it;
         // }
         // console.log(sum);
-        //2. 找出所有字符串,连接
-        for (let i = 0; i < list.length; i++) {
-            let it = list[i];
-            if (typeof it != 'string')
-                continue;
 
-            str += it;
-        }
-        console.log(str);
+        // for (let i = 0; i < list.length; i++) {
+        //     let item = list[i];
+
+        //     if (typeof item != 'number')
+        //         continue;
+
+        //     sum += item;
+        // }
+        // console.log(sum);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     if (typeof list[i] != 'number')
+        //         continue;
+        //     sum += list[i];
+        // }
+        // console.log(sum);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'number')
+        //         continue;
+        //     sum += it;
+        // }
+        // console.log(sum);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'number')
+        //         continue;
+
+        //     sum += it;
+        // }
+        // console.log(sum);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'number')
+        //         continue;
+
+        //     sum = sum + it;
+        // }
+        // console.log(sum);
+
+        //2. 找出所有字符串,连接
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'string')
+        //         continue;
+
+        //     str += it;
+        // }
+        // console.log(str);
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'string')
+        //         continue;
+
+        //     str = str + it;
+        // }
+        // console.log(str);
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'string')
+        //         continue;
+
+        //     str += it;
+        // }
+        // console.log(str);
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'string')
+        //         continue;
+
+        //     str += it;
+        // }
+        // console.log(str);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (typeof it != 'string')
+        //         continue;
+
+        //     str = str + it;
+        // }
+        // console.log(str);
+        let sum = 0;
+        let list = [1, 3, 2, 7, 6];
+        let arr = [];
+        //找出其中所有的偶数，并求出它们的和。
+        // for (let i = 0; i < list.length; i++) {
+        //     let item = list[i];
+        //     if (item % 2)
+        //         continue;
+
+        //     sum += item;
+        // }
+        // console.log(sum);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     if (list[i] % 2)
+        //         continue;
+
+        //     sum = sum + list[i];
+        // }
+        // console.log(sum);
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (it % 2)
+        //         continue;
+
+        //     sum = sum + it;
+        // }
+        // console.log(sum);
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (it % 2)
+        //         continue;
+
+        //     sum += it;
+        // }
+        // console.log(sum);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     if (list[i] % 2)
+        //         continue;
+
+        //     sum += list[i];
+        // }
+        // console.log(sum);
+        // for (let i = 0; i < list.length; i++) {
+        //     if (!(list[i] % 2))
+        //         sum += list[i];
+        // }
+        // console.log(sum);
+
+        //找出其中所有的奇数，并将它们放到一个新数组里。
+        // for (let i = 0; i < list.length; i++) {
+        //     if (!(list[i] % 2))
+        //         continue;
+        //     arr.push(list[i]);
+        // }
+        // console.log(arr);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+        //     if (!(it % 2))
+        //         continue;
+
+        //     arr.push(it);
+        // }
+        // console.log(arr);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     let it = list[i];
+
+        //     if (!(it % 2))
+        //         continue;
+
+        //     arr.push(it);
+        // }
+        // console.log(arr);
+        // for (let i = 0; i < list.length; i++) {
+        //     let item = list[i];
+        //     if (!(item % 2))
+        //         continue;
+        //     arr.push(item);
+        // }
+        // console.log(arr);
+        // for (let i = 0; i < list.length; i++) {
+        //     let item = list[i];
+        //     if (!(item % 2))
+        //         continue;
+
+        //     arr.push(item);
+        // }
+        // console.log(arr);
+
+        // for (let i = 0; i < list.length; i++) {
+        //     if ((list[i] % 2))
+        //         arr.push(list[i]);
+
+        // }
+        // console.log(arr);
+        // for(let i = 0; i<list.length;i++){
+        //     if(list[i]%2)
+        //     arr.push(list[i]);
+        // }
+        // console.log(arr);
     })();
+
+// ;
+// (function () {
+//     'use strict';
+//     let level1 = '第一级: ';
+//     let level2 = '第二级: ';
+//     let level3 = '第三级: ';
+//     let list = ['a1', 'a2', ['b1', 'b2', ['c1', 'c2', 'c3'], 'b3'], 'a3'];
+
+//     //第一级遍历
+//     for (let i = 0; i < list.length; i++) {
+//         let item = list[i];
+
+//         //如果是字符串
+//         if (typeof item == 'string') {
+//             //就追加到当前层级到字符串上
+//             level1 += item + ' ';
+//         } else { //否则就是数组, 继续循环数组;
+//             //第二级遍历
+//             for (let j = 0; j < item.length; j++) {
+//                 let item2 = item[j];
+
+//                 if (typeof item2 == 'string') {
+//                     level2 += item2 + ' ';
+//                 } else { //否则 最内层数组,遍历
+
+//                     for (let k = 0; k < item2.length; k++) {
+//                         let item3 = item2[k];
+
+//                         level3 += item3 + ' ';
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     console.log(level1);
+//     console.log(level2);
+//     console.log(level3);
+// })();
+// ;
+// (function () {
+//     'use strict';
+//     let level1 = '第一级: ';
+//     let level2 = '第二级: ';
+//     let level3 = '第三级: ';
+//     let list = ['a1', 'a2', ['b1', 'b2', ['c1', 'c2', 'c3'], 'b3'], 'a3'];
+
+//     //第一级遍历
+//     for (let i = 0; i < list.length; i++) {
+//         //如果是字符串,追加到当前层的字符串上;
+//         let it = list[i];
+//         if (typeof it == 'string') {
+//             level1 += it + ' ';
+//         } else { //否则就是数组,继续遍历
+//             //第二级遍历
+//             for (let j = 0; j < it.length; j++) {
+//                 let it2 = it[j];
+//                 if (typeof it2 == 'string') {
+//                     level2 += it2 + ' ';
+//                 } else {
+//                     //第三级遍历
+//                     for (let k = 0; k < it2.length; k++) {
+//                         let it3 = it2[k];
+//                         level3 += it3 + ' ';
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     console.log(level1);
+//     console.log(level2);
+//     console.log(level3);
+// })();
+// ;
+// (function () {
+//     'use strict';
+//     let level1 = '第一级: ';
+//     let level2 = '第二级: ';
+//     let level3 = '第三级: ';
+//     let list = ['a1', 'a2', ['b1', 'b2', ['c1', 'c2', 'c3'], 'b3'], 'a3'];
+//     for (let
+//             i = 0; i < list.length; i++) {
+//         if (typeof list[i] == 'string') {
+//             level1 = level1 + list[i] + ' ';
+//         } else {
+//             for (let j = 0; j < list[i].length; j++) {
+//                 if (typeof list[i][j] == 'string') {
+//                     level2 = level2 + list[i][j] + ' ';
+//                 } else {
+//                     for (let k = 0; k < list[i][j].length; k++) {
+//                         level3 = level3 + list[i][j][k] + ' ';
+//                     }
+//                 }
+
+//             }
+//         }
+//     }
+//     console.log(level1);
+//     console.log(level2);
+//     console.log(level3);
+// })();
+// ;
+// (function () {
+//     'use strict';
+//     let level1 = '第一级: ';
+//     let level2 = '第二级: ';
+//     let level3 = '第三级: ';
+//     let list = ['a1', 'a2', ['b1', 'b2', ['c1', 'c2', 'c3'], 'b3'], 'a3'];
+
+//     for (let i = 0; i < list.length; i++) {
+//         if (typeof list[i] == 'string') {
+//             level1 = level1 + list[i] + ' ';
+//         } else {
+//             for (let j = 0; j < list[i].length; j++) {
+//                 if (typeof list[i][j] == 'string') {
+//                     level2 += list[i][j] + ' ';
+//                 } else {
+//                     for (let k = 0; k < list[i][j].length; k++) {
+//                         level3 += list[i][j][k] + ' ';
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     console.log(level1);
+//     console.log(level2);
+//     console.log(level3);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+//     let level1 = '第一级: ';
+//     let level2 = '第二级: ';
+//     let level3 = '第三级: ';
+//     let list = ['a1', 'a2', ['b1', 'b2', ['c1', 'c2', 'c3'], 'b3'], 'a3'];
+
+//     for (let i = 0; i < list.length; i++) {
+//         if (typeof list[i] == 'string') {
+//             level1 += list[i] + ' ';
+//         } else {
+//             for (let j = 0; j < list[i].length; j++) {
+//                 if (typeof list[i][j] == 'string') {
+//                     level2 += list[i][j] + ' ';
+//                 } else {
+//                     for (let k = 0; k < list[i][j].length; k++) {
+//                         level3 += list[i][j][k] + ' ';
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     console.log(level1);
+//     console.log(level2);
+//     console.log(level3);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+//     let level1 = '第一级: ';
+//     let level2 = '第二级: ';
+//     let level3 = '第三级: ';
+//     let list = ['a1', 'a2', ['b1', 'b2', ['c1', 'c2', 'c3'], 'b3'], 'a3'];
+
+//     for (let i = 0; i < list.length; i++) {
+//         if (typeof list[i] == 'string') {
+//             level1 += list[i] + ' ';
+//         } else {
+//             for (let j = 0; j < list[i].length; j++) {
+//                 if (typeof list[i][j] == 'string') {
+//                     level2 += list[i][j] + ' ';
+//                 } else {
+//                     for (let k = 0; k < list[i][j].length; k++) {
+//                         level3 += list[i][j][k] + ' ';
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     console.log(level1);
+//     console.log(level2);
+//     console.log(level3);
+// })();
+;
+(function () {
+    'use strict';
+    // let arr = [];
+    // let list = [{
+    //         name: '王花花',
+    //         salary: 100,
+    //     },
+    //     {
+    //         name: '李拴蛋',
+    //         salary: 200,
+    //     },
+    //     {
+    //         name: '牛备备',
+    //         salary: 300,
+    //     },
+    // ];
+
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (user.salary > 100) {
+    //         arr.push(user.name);
+    //     }
+    // }
+    // console.log(arr);
+
+    // let list = [{
+    //         name: '王花花',
+    //         salary: 100,
+    //     },
+    //     {
+    //         name: '李拴蛋',
+    //         salary: 200,
+    //     },
+    //     {
+    //         name: '牛备备',
+    //         salary: 300,
+    //     },
+    // ];
+
+    // let result = [];
+
+    // for (let i = 0; i < list.length; i++) {
+    //     let it = list[i];
+
+    //     // 如果工资小于等于100，就直接跳过
+    //     if (it.salary <= 100)
+    //         continue;
+
+    //     // 否则就推到数组里
+    //     result.push(it);
+    // }
+
+    // console.log(result);
+    //     let list = [{
+    //             name: '王花花',
+    //             salary: 100,
+    //         },
+    //         {
+    //             name: '李拴蛋',
+    //             salary: 200,
+    //         },
+    //         {
+    //             name: '牛备备',
+    //             salary: 300,
+    //         },
+    //     ];
+    //     let arr = [];
+    //     for (let i = 0; i < list.length; i++) {
+    //         let user = list[i];
+    //         if (user.salary <= 100)
+    //             continue;
+
+    //         arr.push(user);
+    //     }
+    //     console.log(arr);
+
+    // let list = [{
+    //         name: '王花花',
+    //         salary: 100,
+    //     },
+    //     {
+    //         name: '李拴蛋',
+    //         salary: 200,
+    //     },
+    //     {
+    //         name: '牛备备',
+    //         salary: 300,
+    //     },
+    // ];
+    // let arr = [];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (user.salary <= 100)
+    //         continue;
+
+    //     arr.push(user);
+    // }
+    // console.log(arr);
+
+    // let list = [{
+    //         name: '王花花',
+    //         salary: 100,
+    //     },
+    //     {
+    //         name: '李拴蛋',
+    //         salary: 200,
+    //     },
+    //     {
+    //         name: '牛备备',
+    //         salary: 300,
+    //     },
+    // ];
+    // let arr = [];
+    // for (let i = 0; i < list.length; i++) {
+    //     if (list[i].salary <= 100)
+    //         continue;
+
+    //     arr.push(list[i]);
+    // }
+    // console.log(arr);
+
+    // let list = [{
+    //         name: '王花花',
+    //         salary: 100,
+    //     },
+    //     {
+    //         name: '李拴蛋',
+    //         salary: 200,
+    //     },
+    //     {
+    //         name: '牛备备',
+    //         salary: 300,
+    //     },
+    // ];
+    // let arr = [];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (user.salary <= 100)
+    //         continue;
+
+    //     arr.push(user.name);
+    // }
+    // console.log(arr);
+
+    // let list = [{
+    //         name: '王花花',
+    //         salary: 100,
+    //     },
+    //     {
+    //         name: '李拴蛋',
+    //         salary: 200,
+    //     },
+    //     {
+    //         name: '牛备备',
+    //         salary: 300,
+    //     },
+    // ];
+    // let arr = [];
+    // for (let i = 0; i < list.length; i++) {
+    //     if (list[i].salary <= 100)
+    //         continue;
+    //     arr.push(list[i]);
+    // }
+    // console.log(arr);
+
+
+    // let list = [{
+    //         name: '王花花',
+    //         salary: 100,
+    //     },
+    //     {
+    //         name: '李拴蛋',
+    //         salary: 200,
+    //     },
+    //     {
+    //         name: '牛备备',
+    //         salary: 300,
+    //     },
+    // ];
+    // let arr = [];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (user.salary <= 100)
+    //         continue;
+
+    //     arr.push(user.name)
+    // }
+    // console.log(arr)
+})();
+
+;
+(function () {
+    'use strict';
+    // let arr = []
+    // let list = [{
+    //         name: '王花花'
+    //     },
+    //     {
+    //         name: '王拴蛋'
+    //     },
+    //     {
+    //         name: '牛备备'
+    //     },
+    // ];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (!user.name.startsWith('王'))
+    //         continue;
+    //     arr.push(user);
+    // }
+    // console.log(arr);
+
+    // let arr = [];
+    // let list = [{
+    //         name: '王花花'
+    //     },
+    //     {
+    //         name: '王拴蛋'
+    //     },
+    //     {
+    //         name: '牛备备'
+    //     },
+    // ];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (user.name.startsWith('王'))
+    //         arr.push(user);
+    // }
+    // console.log(arr);
+
+    // let arr = [];
+    // let list = [{
+    //         name: '王花花'
+    //     },
+    //     {
+    //         name: '王拴蛋'
+    //     },
+    //     {
+    //         name: '牛备备'
+    //     },
+    // ];
+    // for (let i = 0; i < list.length; i++) {
+    //     if (!list[i].name.startsWith('王'))
+    //         continue;
+    //     arr.push(list[i]);
+    // }
+    // console.log(arr);
+
+    // let arr = [];
+    // let list = [{
+    //         name: '王花花'
+    //     },
+    //     {
+    //         name: '王拴蛋'
+    //     },
+    //     {
+    //         name: '牛备备'
+    //     },
+    // ];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (user.name.startsWith('王'))
+    //         arr.push(user);
+    // }
+    // console.log(arr);
+
+    // let arr = [];
+    // let list = [{
+    //         name: '王花花'
+    //     },
+    //     {
+    //         name: '王拴蛋'
+    //     },
+    //     {
+    //         name: '牛备备'
+    //     },
+    // ];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (!user.name.startsWith('王'))
+    //         continue;
+    //     arr.push(user);
+    // }
+    // console.log(arr);
+
+    // let arr = [];
+    // let list = [{
+    //         name: '王花花'
+    //     },
+    //     {
+    //         name: '王拴蛋'
+    //     },
+    //     {
+    //         name: '牛备备'
+    //     },
+    // ];
+    // for (let i = 0; i < list.length; i++) {
+    //     let user = list[i];
+    //     if (!user.name.startsWith('王'))
+    //         continue;
+    //     arr.push(user.name);
+    // }
+    // console.log(arr);
+
+})();
