f83d3daa5c38e873093d1d3023e07af11dbc00da
zi-mu <zimu.joker@gmail.com>
2020-01-14 23:51:24 +0800

    回调函数 toturial practice 3

--- practice/039-回调函数-机器里面传机器/main.js
+++ practice/039-回调函数-机器里面传机器/main.js
@@ -1,8 +1,68 @@
-function a(arg) {
-    arg();
-}
-a(b);
+//1.
+// function a(arg) {
+//     arg();
+// }
 
-function b() {
-    console.log('i am b.');
-}
+// function b() {
+//     console.log('i am b.');
+// }
+
+// a(b);
+
+// function a(arr,arg){
+//     arg();
+// }
+// function b (){
+//     console.log('i am b');
+// }
+// a(b);
+
+
+//2.
+// function iterator(arr, fn) {
+//     for (let i = 0; i < arr.length; i++) {
+//         fn(arr[i]);
+//     }
+// }
+
+// iterator([12, 3, 4], function (it) {
+//     console.log(it);
+// })
+// function iterator(arr, fn) {
+//     for (let i = 0; i < arr.length; i++) {
+//         fn(arr[i]);
+//     }
+// }
+
+// iterator([1, 2, 3, 4, 5], function (it) {
+//     console.log(it);
+// })
+// function iterator(arr, fn) {
+//     for (let i = 0; i < arr.length; i++) {
+//         fn(arr[i]);
+//     }
+// }
+
+// iterator([1, 2, 3, 4, 4], function (it) {
+//     console.log(it)
+// })
+// function iterator(arr, fn) {
+//     for (let i = 0; i < arr.length; i++) {
+//         fn(arr[i]);
+//     }
+// }
+// iterator([1, 2, 3, 4], function (it) {
+//     console.log(it);
+// })
+
+//3.原生应用 ,数组
+// let arr = ['a', 'b', 'c'];
+// arr.forEach(function (it) {
+//     console.log(it);
+// })
+// arr.forEach(function (it) {
+//     console.log(it);
+// // })
+// arr.forEach(function (it) {
+//     console.log(it);
+// })
