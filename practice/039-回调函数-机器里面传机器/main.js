f2267c33b2726c5cd2b36c7a912f60b4095891e9
zi-mu <zimu.joker@gmail.com>
2020-01-15 00:32:58 +0800

    回调函数课程练习完毕

--- practice/039-回调函数-机器里面传机器/main.js
+++ practice/039-回调函数-机器里面传机器/main.js
@@ -66,3 +66,80 @@
 // arr.forEach(function (it) {
 //     console.log(it);
 // })
+
+//4.应用
+// function shopping(setColor) {
+//     console.log('1.buy vegetables');
+//     console.log('2.buy riyongpin');
+
+//     // 3.买秋裤,问娃想穿啥颜色
+//     let color = setToRed();
+//     console.log('娃想穿' + color + '色的');
+
+//     console.log('4.jiezhang');
+
+// }
+
+// function setToRed() {
+//     return 'Red';
+// }
+// shopping(setToRed);
+
+//5
+
+// function cooking(吃什么, 几个人, 有忌口吗) {
+
+//     console.log('1.买菜: ' + '买跟' + 吃什么() + '相关的食材');
+//     console.log('2.蒸饭 ' + '蒸' + 几个人() + '的饭');
+//     console.log('4.炒菜' + '客人' + 有忌口吗());
+// }
+
+// function 红烧肉() {
+//     return '红烧肉';
+// }
+
+// function 数人() {
+//     let myguest = 2;
+//     let taguest = 3
+//     return myguest + taguest;
+// }
+
+// function 询问忌口() {
+//     let myguest = '不吃香菜';
+//     let taguest = '不吃辣椒';
+//     return [myguest, taguest]
+// }
+// cooking(红烧肉, 数人, 询问忌口);
+
+// function shopping(setcolor) {
+//     console.log('1.mai cai');
+//     console.log('2.mai yongpin');
+
+//     console.log('3.mai qiuke ,wen xia wa xiang mai shenmeyangsede');
+//     let color = setcolor();
+//     console.log('waxiangmai    ' + color + '   sede');
+
+//     console.log('4.jiezhang');
+// }
+// shopping(function () {
+//     return 'red';
+// })
+
+
+//5.
+// function cooking(what, which, isorno) {
+//     console.log('买菜,买跟' + what() + '相关的菜');
+//     console.log('蒸 ' + which() + ' 的饭');
+//     console.log('客人' + isorno());
+// }
+// cooking(function () {
+//     return '红烧肉';
+// }, function () {
+//     let myguest = 5;
+//     let fr = 6;
+//     return myguest + fr;
+// }, function () {
+//     let myguest = '不吃辣椒';
+//     let fr = '不吃香菜';
+//     return [myguest, fr];
+// })
