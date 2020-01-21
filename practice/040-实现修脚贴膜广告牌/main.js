408e5e19fadf30602d1731d54e7d7d6168417572
zi-mu <zimu.joker@gmail.com>
2020-01-22 01:23:15 +0800

    实现修脚贴膜广告牌-100%

--- practice/040-实现修脚贴膜广告牌/main.js
+++ practice/040-实现修脚贴膜广告牌/main.js
@@ -196,24 +196,388 @@
 //     }, 1000);
 // })();
 
-;
-(function () {
-    'use strict';
+// ;
+// (function () {
+//     'use strict';
 
-    let board = document.querySelector('.board');
-    let inner = board.querySelector('.inner')
-    let size = 1;
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner')
+//     let size = 1;
 
-    setInterval(() => {
-        let visible = getComputedStyle(inner).opacity == '1';
-        if (visible) {
-            inner.style.opacity = 0;
-            board.style.borderColor = 'transparent';
-        } else {
-            inner.style.opacity = 1;
-            board.style.borderColor = 'inherit';
-            size < 3 ? size++ : size = 1;
-            inner.style.fontSize = 2 * size + 'rem';
-        }
-    }, 800);
-})();
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             size < 3 ? size++ : size = 1;
+//             inner.style.fontSize = 2 * size + 'rem';
+//         }
+//     }, 800);
+// })();
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let size = 1;
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             size < 3 ? size++ : size = 1;
+//             inner.style.fontSize = 2 * size + 'rem';
+//         }
+//     }, 800);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let size = 1;
+
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             size < 3 ? size++ : size = 1;
+//             inner.style.fontSize = size + 'rem';
+//         }
+//     }, 800);
+// })();
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let size = 1;
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             size < 3 ? size++ : size = 1;
+//             inner.style.fontSize = 2 * size + 'rem';
+//         }
+//     }, 800);
+// })();
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 以整个文档为范围选中.board
+// const board = document.querySelector('.board');
+// // 以.board为范围选中.inner
+// const inner = board.querySelector('.inner');
+
+// // 当前是否为修脚
+// let pedicure = true;
+
+// // 设置间隔
+// setInterval(function () {
+//   // 声明广告牌文字不确定，先声明
+//   let text;
+
+//   // .inner是否可见
+//   let visible = getComputedStyle(inner).opacity == '1';
+
+//   // 如果可见
+//   if (visible) {
+//     // 就隐藏它（让其透明）
+//     inner.style.opacity = 0;
+//     // 同时隐藏边框（让其透明）
+//     board.style.borderColor = 'transparent';
+//   } else { // 否则
+//     // 显示文字
+//     inner.style.opacity = 1;
+
+//     // 显示边框
+//     board.style.borderColor = 'inherit';
+
+//     // 通过pedicure的状态判断当前应该显示的文字
+//     pedicure ? text = '修脚' : text = '贴膜';
+
+//     // 设置内部文字
+//     inner.innerText = text;
+
+//     // 将状态倒置，为下一次做准备
+//     pedicure = !pedicure;
+//   }
+// }, 500);
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let pedicure = true;
+//     setInterval(() => {
+//         let text;
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             pedicure ? text = '修脚贴膜' : text = '按摩';
+//             inner.innerText = text;
+//             pedicure = !pedicure
+//         }
+//     }, 800);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let pedicure = true;
+
+//     setInterval(() => {
+//         let text;
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             pedicure ? text = '修脚贴膜' : text = '按摩洗澡';
+//             inner.innerText = text;
+//             pedicure = !pedicure;
+//         }
+//     }, 800);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+
+//     let pedicure = true;
+//     setInterval(() => {
+//         let text;
+//         let visible = inner.style.opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             pedicure ? text = '修脚贴膜' : text = '按摩洗澡';
+//             inner.innerText = text;
+//             pedicure = !pedicure;
+//         }
+//     }, 800);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let pedicure = true;
+
+//     setInterval(() => {
+//         let text;
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             pedicure ? text = '修脚贴膜' : text = '洗澡按摩';
+//             inner.innerText = text;
+//             pedicure = !pedicure;
+//         }
+//     }, 800);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let pedicure = true;
+
+//     setInterval(() => {
+//         let text;
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             pedicure ? text = '修脚贴膜' : text = '洗澡按摩';
+//             inner.innerText = text;
+//             pedicure = !pedicure;
+//         }
+//     }, 800);
+// })();
+
+//+++++++++++++++++++++++++++++=======================
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+
+//     let list = ['修脚', '上市申报', '擀面皮', '洗澡按摩']
+//     let current = 0;
+//     inner.innerText = list[current];
+
+//     setInterval(() => {
+
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             current++;
+//             if (current >= list.length)
+//                 current = 0;
+//             inner.innerText = list[current]
+//         }
+
+//     }, 800);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let list = ['a', 'b', 'c', 'd'];
+//     let current = 0;
+//     inner.innerText = list[current];
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent'
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             current++;
+//             if (current >= list.length) {
+//                 current = 0;
+//             }
+//             inner.innerText = list[current];
+//         }
+//     }, 900);
+// })();
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let list = ['apple', 'orange', 'bananas'];
+//     let current = 0;
+
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent';
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             current++;
+//             if (current >= list.length) {
+//                 current = 0;
+//             }
+//             inner.innerText = list[current];
+//         }
+//     }, 800);
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let list = ['按摩', '洗澡', '修脚'];
+//     let current = 0;
+
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent'
+//         } else {
+//             inner.style.opacity = 1;
+//             board.style.borderColor = 'inherit';
+//             current++;
+//             if (current >= list.length) {
+//                 current = 0;
+//             }
+//             inner.innerText = list[current];
+//         }
+//     }, 800)
+// })();
+
+// ;
+// (function () {
+//     'use strict';
+
+//     let board = document.querySelector('.board');
+//     let inner = board.querySelector('.inner');
+//     let list = ['按摩', '洗澡', '修脚'];
+//     let current = 0;
+//     let color = ['red', 'lightgreen', 'orange']
+
+//     setInterval(() => {
+//         let visible = getComputedStyle(inner).opacity == '1';
+//         if (visible) {
+//             inner.style.opacity = 0;
+//             board.style.borderColor = 'transparent'
+//         } else {
+//             inner.style.opacity = 1;
+
+//             current++;
+//             if (current >= color.length) {
+//                 current = 0;
+//             }
+//             board.style.borderColor = color[current];
+
+//             if (current >= list.length) {
+//                 current = 0;
+//             }
+//             inner.innerText = list[current];
+//             inner.style.color = color[current];
+//         }
+//     }, 800)
+// })();
