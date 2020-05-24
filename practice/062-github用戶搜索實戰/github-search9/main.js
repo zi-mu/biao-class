a8df3df8fcc2728bb31ce1d0e63bc60101675dde
zi-mu <zimu.joker@gmail.com>
2020-05-24 22:07:26 +0800

    github-search9

---
+++ practice/062-github用戶搜索實戰/github-search9/main.js
@@ -0,0 +1,53 @@
+;
+(function () {
+    'use strict';
+
+    let form = document.getElementById('search-form');
+    let input = form.querySelector('[name=keyword]');
+    let userList = document.getElementById('user-list');
+
+    boot();
+
+    function boot() {
+        bindEvents();
+    }
+
+    function bindEvents() {
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            let keyword = input.value;
+            search(keyword);
+        })
+    }
+
+    function search(keyword) {
+        let http = new XMLHttpRequest();
+        http.open('get', `https://api.github.com/search/users?q=${keyword}`);
+        http.send();
+
+        http.addEventListener('load', () => {
+            let json = http.responseText;
+            let data = JSON.parse(json);
+            render(data);
+        })
+    }
+
+    function render(data) {
+        userList.innerHTML = '';
+        data.items.forEach(it => {
+            let item = document.createElement('div');
+            item.classList.add('item');
+            item.innerHTML = `
+            <div class='avatar'>
+                    <img src='${it.avatar_url}'>
+                </div>
+
+                <div class='detail'>
+                    <strong>${it.login}</strong>
+                    <div><a target='_blank' href='${it.html_url}'>${it.html_url}</a></div>
+                </div>
+            `;
+            userList.appendChild(item);
+        });
+    }
+})();
